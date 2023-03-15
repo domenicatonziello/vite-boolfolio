55b0efceab82e4c8bb6af8c8fe1103376a970da6
domenicatonziello <domenica.tonziello10@gmail.com>
2023-03-15 16:08:46 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  name: 'App',
+  components: { AppHeader },
+}
+</script>
+
+
+<template>
+  <!-- header -->
+  <app-header></app-header>
+  <!-- main -->
+  <main class="container"></main>
+</template>
+
+
+<style></style>
