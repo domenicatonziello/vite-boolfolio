916395f9038ca3de82c2c97e1b4468a954797a8c
domenicatonziello <domenica.tonziello10@gmail.com>
2023-03-15 17:21:35 +0100

    projectCard component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <script>
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+import ProjectCard from './components/projects/ProjectCard.vue';
+const baseUri = 'http://127.0.0.1:8000/api';
 export default {
   name: 'App',
-  components: { AppHeader },
+  components: { AppHeader, ProjectCard },
+  data() {
+    return {
+      projects: [],
+    }
+  },
+  methods: {
+    getProject() {
+      axios.get(baseUri + '/projects').then((res) => {
+        this.projects = res.data;
+      })
+    }
+  },
+  created() {
+    this.getProject();
+  }
 }
 </script>
 
@@ -11,7 +29,11 @@ export default {
   <!-- header -->
   <app-header></app-header>
   <!-- main -->
-  <main class="container"></main>
+  <main class="container">
+    <div class="row my-5">
+      <project-card v-for="project in projects" :key="project.id" :project="project"></project-card>
+    </div>
+  </main>
 </template>
 
 
---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: { project: Object },
+}
+</script>
+
+
+<template>
+    <div class="col my-3">
+        <div class="card" style="width: 18rem;">
+            <img :src="project.image" class="card-img-top" :alt="project.title">
+            <div class="card-body">
+                <h5 class="card-title">{{ project.title }}</h5>
+                <p class="card-text">{{ project.description }}</p>
+                <div class="buttons">
+                    <a href="#" class="btn btn-primary">Dettagli</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+    height: 100%;
+
+    .card-image {
+        height: 150px;
+
+        img {
+            height: 100%;
+            object-fit: cover;
+        }
+    }
+
+    .card-body {
+        display: flex;
+        flex-direction: column;
+
+        .buttons {
+            margin-top: auto;
+        }
+    }
+}
+</style>
